c371511d8d17e59a47c6a86cc805e65b6ed11bf4
vakkD <dylan.colwill@hotmail.com>
2023-04-17 22:55:02 +1000

    many fixes and implementations of new features

--- chat.py
+++ chat.py
@@ -1,140 +1,249 @@
-#add settings (auto multi)
-#able to use function in another, call multi from plag (put get input into a function) 
+# add settings (auto multi)
 
-__version__ = 'dev'
+# MULTIPLE CHOICE
+# throughout the session, i will give you a series of multiple-choice questions. you will respond with the answer only, no description. do you understand?
 
-import os
-import sys
-import logging
-from revChatGPT.V3 import Chatbot
-from termcolor import colored
+#auto update
+
+from tqdm import tqdm
 import subprocess
-message = ''
+from termcolor import colored, cprint
+from revChatGPT.V3 import Chatbot
+import logging
+import sys
+import os
+from itertools import cycle
+import threading
+import time
 
-log_path = os.path.join(os.path.dirname(__file__), 'my_log_file.log')
+__version__ = "dev"
+
+message = ""
+
+log_path = os.path.join(os.path.dirname(__file__), "my_log_file.log")
 # Create a FileHandler that appends messages to the log file
-file_handler = logging.FileHandler(log_path, mode='a')
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, handlers=[
-                    file_handler])  # Set up logging with a custom format and the file handler
+file_handler = logging.FileHandler(log_path, mode="a")
+logging.basicConfig(
+    format="%(asctime)s %(message)s", level=logging.INFO, handlers=[file_handler]
+)  # Set up logging with a custom format and the file handler
 
-logging.info('----- ----- ----- ----- -----')
+logging.info("----- ----- ----- ----- -----")
 
 dict = {
-    'plagarism': {
-        'description': 'Attempts to make the text undetectable by reverse ai software',
-        'functions': ['plag', 'plagarism', 'para', 'paraphrase']},
-    'exit': {
-        'description': 'Exits the program',
-        'functions': ['exit', 'close']},
-    'topic expert': {
-        'description': 'Becomes an expert in any topic',
-        'functions': ['topic', 'expert']},
-    'multi line': {
-        'description': "When finished add a '*'",
-        'functions': ['multiline', 'multi', 'long']},
-    'view logs': {
-        'description': 'View the saved conversations',
-        'functions': ['log', 'logs', 'openlog', 'openlogs']},
-    'stop output': {
-        'description': 'ctrl+c',
-        'functions': []}}
+    "plagarism": {
+        "description": "Attempts to make the text undetectable by reverse ai software",
+        "functions": ["plag", "plagarism", "para", "paraphrase"],
+    },
+    "exit": {
+        "description": "Exits the program", 
+        "functions": ["exit", "close"]},
+    
+    "topic expert": {
+        "description": "Becomes an expert in any topic",
+        "functions": ["topic", "expert"],
+    },
+    "multi line": {
+        "description": "Allows input of multiple lines, when finished add a '*'",
+        "functions": ["multiline", "multi", "long"],
+    },
+    
+    "multiple choice": {
+        "description": "Answers in the format of multiple choice, auto multi line, finish with --end", 
+        "functions": ["multiple", "choice"]},
+    
+    "view logs": {
+        "description": "View the saved conversations",
+        "functions": ["log", "logs", "openlog", "openlogs"],
+    },
+    "new session": {
+        "description": "Clears the current session and starts new",
+        "functions": ["session", "clear"],
+    },
+    "settings menu": {
+        "description": "Open the settings menu",
+        "functions": ["settings", "options"],
+    },
+    "stop output": {"description": "ctrl+c", "functions": []},
+}
+
+settings={'stream': False}
+
+class Spinner:
+    __default_spinner_symbols_list = ['|-----|', '|#----|', '|-#---|', '|--#--|', '|---#-|', '|----#|']
+
+    def __init__(self, spinner_symbols_list: [str] = None):
+        spinner_symbols_list = spinner_symbols_list if spinner_symbols_list else Spinner.__default_spinner_symbols_list
+        self.__screen_lock = threading.Event()
+        self.__spinner = cycle(spinner_symbols_list)
+        self.__stop_event = False
+        self.__thread = None
+
+    def get_spin(self):
+        return self.__spinner
+
+    def start(self, spinner_message: str):
+        self.__stop_event = False
+        time.sleep(0.1)
+
+        def run_spinner(message):
+            while not self.__stop_event:
+                print("\r{message} {spinner}".format(message=message, spinner=next(self.__spinner)), end="")
+                time.sleep(0.1)
+
+            self.__screen_lock.set()
+
+        self.__thread = threading.Thread(target=run_spinner, args=(spinner_message,), daemon=True)
+        self.__thread.start()
+
+    def stop(self):
+        self.__stop_event = True
+        if self.__screen_lock.is_set():
+            self.__screen_lock.wait()
+            self.__screen_lock.clear()
+            print("\r", end="")
+
+        print("\r", end="")
+
+def get_input(multi=False):
+    cprint("input: ", "red", attrs=["bold"], end="")
+    message = input()
+    if message.startswith("--"):
+        # passes a slice of the message string, starting from the 3rd character
+        message = functions(message[2:])
+        if not message == False:
+            return message
+        else:
+            return False
+    elif message.startswith("-"):
+        cprint(
+            "warning: functions have two dashes, --", "white", "on_red", attrs=["bold"]
+        )
+    elif not message:
+        colored("message is empty", "white", "on_red", attrs=["bold"])
+        return False
+    logging.info(f"INPUT: {message}")
+    return message
 
 
 def functions(func):
     global message
+    global chat
+    func = func.lower()
 
-    if func == 'help':
-        print('\nFunctions')
+    if func == "help":
+        print("\nFunctions")
         for key, values in dict.items():
-            print(f'{key.capitalize()}: ', end='')
-            for i, value in enumerate(values['functions']):
-                if i == len(values['functions']) - 1:
-                    print(f'--{value}', end='')
+            print(f"{key.capitalize()}: ", end="")
+            for i, value in enumerate(values["functions"]):
+                if i == len(values["functions"]) - 1:
+                    print(f"--{value}", end="")
                 else:
-                    print(f'--{value}', end=", ")
+                    print(f"--{value}", end=", ")
             print(f"\n\t{values['description']}\n")
         return False
 
-    elif func == 'exit':
+    elif func == "exit":
         os._exit(0)
 
-    elif func in dict['view logs']['functions']:
-        subprocess.Popen(['notepad.exe', log_path])
+    elif func in dict["settings menu"]["functions"]:
+        # input=get_input()
+        print("in development")
         return False
 
-    elif func in dict['plagarism']['functions']:
-        message = f'''I will give you some text and using the promt rewrite it:
-Rewrite it using no repetitive words, and change up the sentence structure. A essay must contain high perplexity and burstiness for it to seem natural.
-Text:
-"{''.join(str(input(f"{colored('input text: ', color='white', attrs=['bold'])}")))}"'''
-        return True
+    elif func in dict["view logs"]["functions"]:
+        subprocess.Popen(["notepad.exe", log_path])
+        return False
 
-    elif func in dict['topic expert']['functions']:
-        topic = input(
-            f"{colored('input topic: ', color='white', attrs=['bold'])}")
-        message = f'''I want you to act as an expert in "{topic}". You have all the information about anything to do with "{topic}".
-I will ask you questions about different topics within "{topic}" and you will provide me with clear, concise, and accurate information.
-Ensure that you only respond back with answers that you are fully confident in.
-Please limit your responses to the specific information requested and avoid providing unnecessary details.'''
-        return True
+    elif func in dict["plagarism"]["functions"]:
+        message = get_input()
+        spinner.start('generating response')
+        chat.ask(
+            "I'm going to give you some information before asking you to write an article. Do you understand?"
+        )
+        chat.ask(
+            """When it comes to writing content, two factors are crucial, "perplexity" and "burstiness." Perplexity measures the complexity of text. Separately, burstiness compares the variations of sentences. Humans tend to write with great burstiness, for example, with some longer or complex sentences alongside shorter sentences. AI sentences tend to be more uniform. Therefore, when writing the following content I am going to ask you to create, I need it to have a good amount of perplexity and burstiness. Do you understand?"""
+        )
+        chat.ask(
+            f'''using the concepts written previously, rewrite this article with a high degree of perplexity and burstiness:"{''.join(str(message))}"'''
+        )
+        spinner.stop()
+        return "Rewrite this article changing 50% of nouns and verbs for similar ones and rewrite verb usages to add natural sounding variations in complexity but retain the same tense and overall meaning."
 
-    elif func in dict['multi line']['functions']:
-        message = ' '
-        while not message[-1] == '*':
-            message += ('\n' +
-                        str(input(f"{colored('input: ', color='white', attrs=['bold'])}")))
-                        #str(get_input()))
-        message = message[:-1]
-        return True
+    elif func in dict['multiple choice']['functions']:
+        print('in development')
+        return False
+        chat.ask('throughout the session, i will give you a series of multiple-choice questions. you will respond with the answer only, no description. do you understand?')
+        message=get_input()
+        while not message.startswith('--'):
+            pass
+        if message.replace('--',''):
+            return False
+    
+    elif func in dict["topic expert"]["functions"]:
+        topic = get_input()
+        message = f"""I want you to act as an expert in "{topic}". You have all the information about anything to do with "{topic}".\nI will ask you questions about different topics within "{topic}" and you will provide me with clear, concise, and accurate information.\nEnsure that you only respond back with answers that you are fully confident in.\nPlease limit your responses to the specific information requested and avoid providing unnecessary details."""
+        return message
+
+    elif func in dict["multi line"]["functions"]:
+        message = " "
+        while not message[-1] == "*":
+            message += "\n" + str(get_input())
+        return message[:-1]
+
+    elif func in dict["new session"]["functions"]:
+        logging.info("----- ----- ----- ----- -----")
+        cprint("starting new session", "white", "on_red", attrs=["bold"])
+        chat = Chatbot(api_key=os.getenv("OPENAI_API_KEY"))
+        return False
 
     else:
-        print('Function does not exist, --help for info')
+        cprint("Function does not exist, --help for info", "white", "on_red", attrs=["bold"])
         return False
 
 
 def clear_lines(lines_to_clear):
     for i in range(lines_to_clear):
-        sys.stdout.write('\x1b[1A')  # Move the cursor one line up
-        sys.stdout.write('\x1b[2K')  # Clear the line
+        sys.stdout.write("\x1b[1A")  # Move the cursor one line up
+        sys.stdout.write("\x1b[2K")  # Clear the line
     sys.stdout.flush()  # Flush the output buffer to ensure the changes are displayed
 
-def get_input(multi=False):
-    message = input(f"{colored('input: ', color='white', attrs=['bold'])}")
-    # if multi==True:
-    #     return message
-    if message.startswith('--'):
-        # passes a slice of the message string, starting from the 3rd character
-        if functions(message[2:]):
-            return message
-        else:
-            return False
-    elif message.startswith('-'):
-        print(colored('warning: functions have two dashes, --',
-              color='white', attrs=['bold']))
-    elif not message:
-        print(colored('message is empty', color='white', attrs=['bold']))
-        return False
-    logging.info(f'INPUT: {message}')
 
-print('connecting')
+print("connecting")
 chat = Chatbot(api_key=os.getenv("OPENAI_API_KEY"))
 clear_lines(1)
+spinner=Spinner()
 
 while True:
-    output = ''
-    message=get_input()
-    
-    if message==False:
-        continue 
-
-    print(colored('output: ', color='white', attrs=['bold']), end='')
     try:
-        for data in chat.ask_stream(message):
-            print(data, end="", flush=True)
-            output += str(data)
-        logging.info(f'OUTPUT: {output}\n')
+        output = ""
+        message = get_input()
+
+        if message == False:
+            continue
     except KeyboardInterrupt:
-        print(colored('\noutput interupted by user',
-              color='white', attrs=['bold']))
-        logging.info(f'OUTPUT: {output}\nOUTPUT INTERUPTED BY USER\n')
-    print('')
+        cprint("\nuse --exit to close", "white", "on_red", attrs=["bold"])
+        continue
+
+    if settings['stream']==True:    
+        try:
+            cprint("output: ", "red", attrs=["bold"], end="")
+            for data in chat.ask_stream(message):
+                print(data, end="", flush=True)
+                output += str(data)
+            logging.info(f"OUTPUT: {output}\n")
+            print("")
+        except KeyboardInterrupt:
+            cprint("\noutput interupted by user", "white", "on_red", attrs=["bold"])
+            logging.info(f"OUTPUT: {output}\nOUTPUT INTERUPTED BY USER\n")
+    else:
+        try:
+            spinner.start('generating response')
+            reponse=chat.ask(message)
+            output += str(reponse)
+            spinner.stop()
+            cprint("output: ", "red", attrs=["bold"], end="")
+            print(reponse)
+            logging.info(f"OUTPUT: {output}\n")
+        except KeyboardInterrupt:
+            cprint("\noutput interupted by user", "white", "on_red", attrs=["bold"])
+            logging.info(f"OUTPUT: {output}\nOUTPUT INTERUPTED BY USER\n")
