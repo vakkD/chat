75ec977a6be6d913df22e7e725ee3fd7a41bd1c9
Dylan Colwill <dylan.colwill@hotmail.com>
2023-04-19 01:21:31 +1000

    settings menu added, able to toggle options

--- chat.py
+++ chat.py
@@ -1,10 +1,13 @@
-# add settings (auto multi)
+# complete settings functions
 
 # MULTIPLE CHOICE
 # throughout the session, i will give you a series of multiple-choice questions. you will respond with the answer only, no description. do you understand?
 
-#auto update
+# auto update
 
+# make chat not continue last request when cancel output
+import configparser
+import io
 from tqdm import tqdm
 import subprocess
 from termcolor import colored, cprint
@@ -18,14 +21,32 @@ import time
 
 __version__ = "dev"
 
+clear = lambda: os.system("cls")
+
 message = ""
 
-log_path = os.path.join(os.path.dirname(__file__), "my_log_file.log")
+log_path = os.path.join(os.path.dirname(__file__), "log.log")
 # Create a FileHandler that appends messages to the log file
 file_handler = logging.FileHandler(log_path, mode="a")
 logging.basicConfig(
     format="%(asctime)s %(message)s", level=logging.INFO, handlers=[file_handler]
 )  # Set up logging with a custom format and the file handler
+
+# region settings
+default_settings = {"stream": True, "auto multi": False, "auto update": False}
+if not os.path.exists("config.ini"):
+    config = configparser.ConfigParser()
+    config["settings"] = default_settings
+    with open("config.ini", "w") as configfile:
+        config.write(configfile)
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+settings = {}
+for key, value in default_settings.items():
+    settings[key] = config.getboolean("settings", key, fallback=value)
+# endregion settings
 
 logging.info("----- ----- ----- ----- -----")
 
@@ -34,10 +55,7 @@ dict = {
         "description": "Attempts to make the text undetectable by reverse ai software",
         "functions": ["plag", "plagarism", "para", "paraphrase"],
     },
-    "exit": {
-        "description": "Exits the program", 
-        "functions": ["exit", "close"]},
-    
+    "exit": {"description": "Exits the program", "functions": ["exit", "close"]},
     "topic expert": {
         "description": "Becomes an expert in any topic",
         "functions": ["topic", "expert"],
@@ -46,11 +64,10 @@ dict = {
         "description": "Allows input of multiple lines, when finished add a '*'",
         "functions": ["multiline", "multi", "long"],
     },
-    
     "multiple choice": {
-        "description": "Answers in the format of multiple choice, auto multi line, finish with --end", 
-        "functions": ["multiple", "choice"]},
-    
+        "description": "Answers in the format of multiple choice, auto multi line, finish with --end",
+        "functions": ["multiple", "choice"],
+    },
     "view logs": {
         "description": "View the saved conversations",
         "functions": ["log", "logs", "openlog", "openlogs"],
@@ -61,18 +78,28 @@ dict = {
     },
     "settings menu": {
         "description": "Open the settings menu",
-        "functions": ["settings", "options"],
+        "functions": ["settings", "setting", "options"],
     },
     "stop output": {"description": "ctrl+c", "functions": []},
 }
 
-settings={'stream': True}
 
 class Spinner:
-    __default_spinner_symbols_list = ['|-----|', '|#----|', '|-#---|', '|--#--|', '|---#-|', '|----#|']
+    __default_spinner_symbols_list = [
+        "|-----|",
+        "|#----|",
+        "|-#---|",
+        "|--#--|",
+        "|---#-|",
+        "|----#|",
+    ]
 
     def __init__(self, spinner_symbols_list: [str] = None):
-        spinner_symbols_list = spinner_symbols_list if spinner_symbols_list else Spinner.__default_spinner_symbols_list
+        spinner_symbols_list = (
+            spinner_symbols_list
+            if spinner_symbols_list
+            else Spinner.__default_spinner_symbols_list
+        )
         self.__screen_lock = threading.Event()
         self.__spinner = cycle(spinner_symbols_list)
         self.__stop_event = False
@@ -87,12 +114,19 @@ class Spinner:
 
         def run_spinner(message):
             while not self.__stop_event:
-                print("\r{message} {spinner}".format(message=message, spinner=next(self.__spinner)), end="")
+                print(
+                    "\r{message} {spinner}".format(
+                        message=message, spinner=next(self.__spinner)
+                    ),
+                    end="",
+                )
                 time.sleep(0.1)
 
             self.__screen_lock.set()
 
-        self.__thread = threading.Thread(target=run_spinner, args=(spinner_message,), daemon=True)
+        self.__thread = threading.Thread(
+            target=run_spinner, args=(spinner_message,), daemon=True
+        )
         self.__thread.start()
 
     def stop(self):
@@ -104,9 +138,12 @@ class Spinner:
 
         print("\r", end="")
 
-def get_input(multi=False):
+
+def get_input(function=None):
     cprint("input: ", "red", attrs=["bold"], end="")
     message = input()
+    if not function == None:
+        pass
     if message.startswith("--"):
         # passes a slice of the message string, starting from the 3rd character
         message = functions(message[2:])
@@ -123,6 +160,39 @@ def get_input(multi=False):
         return False
     logging.info(f"INPUT: {message}")
     return message
+
+
+def settings_menu():
+    while True:
+        print("Settings Menu")
+        print("-------------")
+        for key, value in settings.items():
+            print(f"{key.capitalize()}: {value}")
+        print("-------------")
+        setting = input(
+            "Enter the name of the setting you want to modify, or 'exit' to return to the chat: "
+        ).lower()
+        if setting == "exit":
+            clear()
+            break
+        elif setting not in settings:
+            clear()
+            print("Invalid setting name. Please try again.")
+            continue
+        else:
+            print(f"Current value of {setting.capitalize()}: {settings[setting]}")
+            toggle = input("Do you want to toggle this setting? (y/n) ").lower()
+            if toggle == "y":
+                settings[setting] = not settings[setting]
+                print(f"{setting.capitalize()} set to {settings[setting]}")
+                config = configparser.ConfigParser()
+                config.read("config.ini")
+                config.set("settings", setting, str(settings[setting]))
+                with open("config.ini", "w") as configfile:
+                    config.write(configfile)
+            else:
+                print("Setting not changed.")
+        clear()
 
 
 def functions(func):
@@ -146,8 +216,8 @@ def functions(func):
         os._exit(0)
 
     elif func in dict["settings menu"]["functions"]:
-        # input=get_input()
-        print("in development")
+        settings_menu()
+        # print("in development")
         return False
 
     elif func in dict["view logs"]["functions"]:
@@ -156,7 +226,7 @@ def functions(func):
 
     elif func in dict["plagarism"]["functions"]:
         message = get_input()
-        spinner.start('generating response')
+        spinner.start("generating response")
         chat.ask(
             "I'm going to give you some information before asking you to write an article. Do you understand?"
         )
@@ -169,16 +239,18 @@ def functions(func):
         spinner.stop()
         return "Rewrite this article changing 50% of nouns and verbs for similar ones and rewrite verb usages to add natural sounding variations in complexity but retain the same tense and overall meaning."
 
-    elif func in dict['multiple choice']['functions']:
-        print('in development')
+    elif func in dict["multiple choice"]["functions"]:
+        print("in development")
         return False
-        chat.ask('throughout the session, i will give you a series of multiple-choice questions. you will respond with the answer only, no description. do you understand?')
-        message=get_input()
-        while not message.startswith('--'):
+        chat.ask(
+            "throughout the session, i will give you a series of multiple-choice questions. you will respond with the answer only, no description. do you understand?"
+        )
+        message = get_input()
+        while not message.startswith("--"):
             pass
-        if message.replace('--',''):
+        if message.replace("--", ""):
             return False
-    
+
     elif func in dict["topic expert"]["functions"]:
         topic = get_input()
         message = f"""I want you to act as an expert in "{topic}". You have all the information about anything to do with "{topic}".\nI will ask you questions about different topics within "{topic}" and you will provide me with clear, concise, and accurate information.\nEnsure that you only respond back with answers that you are fully confident in.\nPlease limit your responses to the specific information requested and avoid providing unnecessary details."""
@@ -197,7 +269,12 @@ def functions(func):
         return False
 
     else:
-        cprint("Function does not exist, --help for info", "white", "on_red", attrs=["bold"])
+        cprint(
+            "Function does not exist, --help for info",
+            "white",
+            "on_red",
+            attrs=["bold"],
+        )
         return False
 
 
@@ -211,7 +288,7 @@ def clear_lines(lines_to_clear):
 print("connecting")
 chat = Chatbot(api_key=os.getenv("OPENAI_API_KEY"))
 clear_lines(1)
-spinner=Spinner()
+spinner = Spinner()
 
 while True:
     try:
@@ -225,18 +302,19 @@ while True:
         continue
 
     try:
-        if settings['stream']==True:    
+        if settings["stream"] == True:
             cprint("output: ", "red", attrs=["bold"], end="")
             for data in chat.ask_stream(message):
                 print(data, end="", flush=True)
                 output += str(data)
         else:
-            spinner.start('generating response')
-            reponse=chat.ask(message)
+            spinner.start("generating response")
+            reponse = chat.ask(message)
             output += str(reponse)
             spinner.stop()
             cprint("output: ", "red", attrs=["bold"], end="")
             print(reponse)
+        print("")
         logging.info(f"OUTPUT: {output}\n")
     except KeyboardInterrupt:
         cprint("\noutput interupted by user", "white", "on_red", attrs=["bold"])
